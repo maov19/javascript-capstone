3750fba73b469832a5caf3540ef853e52a5b12eb
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-03-14 11:19:34 +0000

    Added header to the page

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 const tailwindcss = require('tailwindcss');
+
 module.exports = {
-    plugins: [
-        'postcss-preset-env',
-        tailwindcss
-    ],
+  plugins: [
+    'postcss-preset-env',
+    tailwindcss,
+  ],
 };
