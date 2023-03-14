913be1b7dc7d00839443b8ac3d9fadeb4a7f9565
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-03-14 09:01:16 +0000

    Webpack successfully setup with tailwind and daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const daisyui = require('daisyui')
+
+module.exports = {
+    content: ['./dist/*.{html, js}'],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [daisyui],
+}
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/template.html'
+        })
+    ]
+}
