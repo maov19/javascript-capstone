7a10de29196ebc1e91256d7ed3ae923871f77ea2
mossa <martin.ossa.8@gmail.com>
2023-03-15 14:56:38 +0100

    Add function to display likes

--- src/modules/displayAllAnime.js
+++ src/modules/displayAllAnime.js
@@ -1,4 +1,5 @@
 import Anime from './Anime.js';
+import Involvement from './involvement.js';
 
 const displayAllAnime = () => {
   const anime = new Anime();
@@ -6,4 +7,28 @@ const displayAllAnime = () => {
     .then((data) => anime.displayAnime(data))
     .catch((error) => console.error(error));
 };
+
+const displayLikes = () => {
+  const likes = new Involvement ();
+  likes.getLikes()
+    .then((data) => anime.displayLikes(data))
+    .catch((error) => console.error(error));
+}
+
+// this is the function im trying to post, but its not working properly
+// async function postMethod(data) {
+//   const apiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps';
+
+//   const response = await fetch(apiUrl, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//   });
+
+//   const responseData = await response.json();
+//   console.log(responseData);
+// }
+
 export default displayAllAnime;
