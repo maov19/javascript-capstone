b262a406a13bb3c34f14776b82c96a26c4bffd1b
mossa <martin.ossa.8@gmail.com>
2023-03-16 20:27:40 +0100

    Fix styling issues

--- src/modules/commentCountTest.test.js
+++ src/modules/commentCountTest.test.js
@@ -3,29 +3,29 @@
  */
 
 const commentCounter = () => {
-    let counter = document.querySelectorAll('.comment').length //this class should be assigned to single comments
-    let displayCounter = document.querySelector('.comment-counter') //insert this in the popups, on top of the comments
-    if (counter === 0) {
-        counter = 'No comments yet'
-    } else {
-        counter = `${counter} comments`
-    }
-    return counter;
-}  
-  describe('Counting comments test', () => {
-    test('counts with 0 items displayed', () => {
+  let counter = document.querySelectorAll('.comment').length; // this class should be assigned to single comments
+  const displayCounter = document.querySelector('.comment-counter'); // insert this in the popups, on top of the comments
+  if (counter === 0) {
+    counter = 'No comments yet';
+  } else {
+    counter = `${counter} comments`;
+  }
+  return counter;
+};
+
+describe('Counting comments test', () => {
+  test('counts with 0 items displayed', () => {
     document.body.innerHTML = "<li class='comment'></li>".repeat(0);
     expect(commentCounter()).toBe('No comments yet');
-    });
-  
-    test('counts with 3 items displayed', () => {
-      document.body.innerHTML = "<li class='comment'></li>".repeat(3);
-      expect(commentCounter()).toBe('3 comments');
-    });
-  
-    test('counts with 100 items displayed', () => {
-      document.body.innerHTML = "<li class='comment'></li>".repeat(100);
-      expect(commentCounter()).toBe('100 comments');
-    });
   });
-  
+
+  test('counts with 3 items displayed', () => {
+    document.body.innerHTML = "<li class='comment'></li>".repeat(3);
+    expect(commentCounter()).toBe('3 comments');
+  });
+
+  test('counts with 100 items displayed', () => {
+    document.body.innerHTML = "<li class='comment'></li>".repeat(100);
+    expect(commentCounter()).toBe('100 comments');
+  });
+});
--- src/modules/countComments.js
+++ src/modules/countComments.js
@@ -1,9 +1,10 @@
 const commentCounter = () => {
-    let counter = document.querySelectorAll('.comment').length //this class should be assigned to single comments
-    let displayCounter = document.querySelector('.comment-counter') //insert this in the popups, on top of the comments
-    if (!comment) {
-        displayCounter.textContent = 'No comments yet'
-    } else {
-        displayCounter.textContent = `${counter} comments`
-    }
-}
+  const counter = document.querySelectorAll('.comment').length; // this class should be assigned to single comments
+  const displayCounter = document.querySelector('.comment-counter'); // insert this in the popups, on top of the comments
+  if (count === 0) {
+    displayCounter.textContent = 'No comments yet';
+  } else {
+    displayCounter.textContent = `${counter} comments`;
+  }
+};
+commentCounter();
